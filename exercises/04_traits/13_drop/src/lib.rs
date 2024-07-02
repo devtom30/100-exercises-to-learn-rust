ad8efb9701dc8287ea5d214d7c3c0d7a4829e95f
x2030280 <thomas.lornet@ratpsmartsystems.com>
2024-07-02 15:08:40 +0200

    done

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,26 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+struct DropBomb {
+    defused: bool
+}
+
+impl DropBomb {
+
+    pub fn defuse(mut self) {
+        self.defused = true;
+    }
+    pub fn new() -> Self {
+        Self { defused: false }
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        assert!(self.defused);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
