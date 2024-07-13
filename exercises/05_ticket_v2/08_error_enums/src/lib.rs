ba8081fc244638b5c135b44f59e6f9acca3c9add
x2030280 <thomas.lornet@ratpsmartsystems.com>
2024-07-13 08:46:57 +0200

    done

--- exercises/05_ticket_v2/08_error_enums/src/lib.rs
+++ exercises/05_ticket_v2/08_error_enums/src/lib.rs
@@ -1,14 +1,33 @@
 // TODO: Use two variants, one for a title error and one for a description error.
 //   Each variant should contain a string with the explanation of what went wrong exactly.
 //   You'll have to update the implementation of `Ticket::new` as well.
-enum TicketNewError {}
+enum TicketNewError {
+    TitleError {message: String},
+    DescriptionError {message: String}
+}
 
 // TODO: `easy_ticket` should panic when the title is invalid, using the error message
 //   stored inside the relevant variant of the `TicketNewError` enum.
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
-fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+fn easy_ticket_old(title: String, description: String, status: Status) -> Ticket {
+    let ticket_result = Ticket::new(title.clone(), description, status.clone());
+    match ticket_result {
+        Err(TicketNewError::DescriptionError { message}) =>
+            easy_ticket(title.clone(), "Description not provided".to_string(), status.clone()),
+        Err(TicketNewError::TitleError { message}) => panic!("{}", message),
+        Ok(ticket) => ticket
+    }
+}
+
+fn easy_ticket(title: String, description: String, status: crate::Status) -> crate::Ticket {
+    Ticket::new(title.clone(), description, status.clone()).map_err(|error| {
+        match error {
+            TicketNewError::DescriptionError { message} =>
+                easy_ticket(title.clone(), "Description not provided".to_string(), status.clone()),
+            TicketNewError::TitleError { message} => panic!("{}", message)
+        }
+    }).unwrap_or(Ticket {title: "default".to_string(), description: "default".to_string(), status: Status::ToDo})
 }
 
 #[derive(Debug, PartialEq)]
@@ -32,16 +51,16 @@ impl Ticket {
         status: Status,
     ) -> Result<Ticket, TicketNewError> {
         if title.is_empty() {
-            return Err("Title cannot be empty".to_string());
+            return Err(TicketNewError::TitleError {message: "Title cannot be empty".to_string()});
         }
         if title.len() > 50 {
-            return Err("Title cannot be longer than 50 bytes".to_string());
+            return Err(TicketNewError::TitleError {message: "Title cannot be longer than 50 bytes".to_string()});
         }
         if description.is_empty() {
-            return Err("Description cannot be empty".to_string());
+            return Err(TicketNewError::DescriptionError {message: "Description cannot be empty".to_string()});
         }
         if description.len() > 500 {
-            return Err("Description cannot be longer than 500 bytes".to_string());
+            return Err(TicketNewError::DescriptionError {message: "Description cannot be longer than 500 bytes".to_string()});
         }
 
         Ok(Ticket {
