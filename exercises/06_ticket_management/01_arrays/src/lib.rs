81224e1e216f80bb05fb3405f95dc17682131519
devtom30 <devtom30@gmail.com>
2024-08-16 16:59:10 +0200

    done

--- exercises/06_ticket_management/01_arrays/src/lib.rs
+++ exercises/06_ticket_management/01_arrays/src/lib.rs
@@ -1,9 +1,12 @@
 // TODO: Flesh out the `WeekTemperatures` struct and its method implementations to pass the tests.
 
+use std::collections::HashMap;
+
 pub struct WeekTemperatures {
-    // TODO
+    temperatures: HashMap<Weekday, i32>
 }
 
+#[derive(Hash, Eq, PartialEq)]
 pub enum Weekday {
     Monday,
     Tuesday,
@@ -16,15 +19,17 @@ pub enum Weekday {
 
 impl WeekTemperatures {
     pub fn new() -> Self {
-        todo!()
+        Self {
+            temperatures: HashMap::new()
+        }
     }
 
     pub fn get_temperature(&self, day: Weekday) -> Option<i32> {
-        todo!()
+        self.temperatures.get(&day).cloned()
     }
 
     pub fn set_temperature(&mut self, day: Weekday, temperature: i32) {
-        todo!()
+        self.temperatures.insert(day, temperature);
     }
 }
 
