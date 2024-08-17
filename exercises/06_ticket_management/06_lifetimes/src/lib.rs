a80da37c25c9346c82bc0d7a7d21d21f836b8119
devtom30 <devtom30@gmail.com>
2024-08-17 18:45:23 +0200

    done

--- exercises/06_ticket_management/06_lifetimes/src/lib.rs
+++ exercises/06_ticket_management/06_lifetimes/src/lib.rs
@@ -36,6 +36,15 @@ impl TicketStore {
     }
 }
 
+impl<'a> IntoIterator for &'a TicketStore {
+    type Item = &'a Ticket;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tickets.iter().map(|ticket: &Ticket| ticket).collect::<Vec<&Ticket>>().into_iter()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
