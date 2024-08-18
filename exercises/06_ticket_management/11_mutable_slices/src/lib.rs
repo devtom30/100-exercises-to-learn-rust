2b5ee2e769614b27f94d30ddf1e86c6c0689dfaf
devtom30 <devtom30@gmail.com>
2024-08-18 11:45:20 +0200

    done

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,8 +1,8 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
-fn lowercase(s: &mut str) -> String {
-    s.to_lowercase()
+fn lowercase(s: &mut str) {
+    s.to_lowercase();
 }
 #[cfg(test)]
 mod tests {
